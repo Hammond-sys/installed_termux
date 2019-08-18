3679f6a020948b28ee1fc385990a16aca14351b8
Start_Hammond <53830640+Hammond-sys@users.noreply.github.com>
2019-08-18 19:49:39 +0700

    Add files via upload

---
+++ installed_termux.sh
@@ -0,0 +1,11 @@
+#bahan bahan termux
+clear
+pkg update
+pkg upgrade
+pkg install git python python2 php cowsay ruby nano vim mc sl x11-repo unstable-repo root-repo screenfetch neofetch
+gem install lolcat
+pip2 install mechanize
+echo "selesai"
+sleep 03
+clear
+login
